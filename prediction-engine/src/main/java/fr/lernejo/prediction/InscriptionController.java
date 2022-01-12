0e94b408c258f5bc5c16208254cf06e3747b6837
Dehbia20 <dehbia.djebarrii@gmail.com>
2022-01-12 23:04:04 +0100

    partie 2

---
+++ prediction-engine/src/main/java/fr/lernejo/prediction/InscriptionController.java
@@ -0,0 +1,46 @@
+package fr.lernejo.prediction;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/inscription")
+public class InscriptionController {
+    private List<Inscription> inscriptions = new ArrayList<>();
+
+    @PostMapping
+    public ResponseEntity<Void> create(@RequestBody Inscription inscription) {
+        if (inscription == null) {
+            return ResponseEntity.unprocessableEntity().build();
+        }
+
+        if (inscription.getUserEmail() == null || "".equals(inscription.getUserEmail())) {
+            return ResponseEntity.unprocessableEntity().build();
+        }
+
+        if (inscription.getUserName() == null || "".equals(inscription.getUserName())) {
+            return ResponseEntity.unprocessableEntity().build();
+        }
+
+        if (inscription.getWeatherExpectation() == null || Arrays.asList("WARMER", "COLDER").stream().noneMatch(s -> s.equals(inscription.getWeatherExpectation()))) {
+            return ResponseEntity.unprocessableEntity().build();
+        }
+
+        if (inscription.getMinimumTemperatureDistance()  < 0 || inscription.getMinimumTemperatureDistance() > 40) {
+            return ResponseEntity.unprocessableEntity().build();
+        }
+        inscriptions.add(inscription);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+
+
+}
---
+++ prediction-engine/src/main/java/fr/lernejo/prediction/TravelController.java
@@ -0,0 +1,23 @@
+package fr.lernejo.prediction;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/travels")
+public class TravelController {
+
+    @GetMapping
+    public ResponseEntity<List<Travel>> suggestion(@RequestParam(name = "userName") String userName) {
+        Travel travel = new Travel();
+        travel.setName("test");
+        travel.setTemperature(12.2f);
+        return ResponseEntity.ok(Arrays.asList(travel));
+    }
+}
